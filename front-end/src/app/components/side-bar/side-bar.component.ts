34bcbdae18439821270666098793bb0ff3da898a
afraklaabdelaziz <afraklaabdelaziz@gmail.com>
2023-04-05 12:12:43 +0000

    ajouter component nav-bar et side-bar

---
+++ front-end/src/app/components/side-bar/side-bar.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { UserService } from 'src/app/services/user-service.service';
+
+@Component({
+  selector: 'app-side-bar',
+  templateUrl: './side-bar.component.html',
+  styleUrls: ['./side-bar.component.css']
+})
+export class SideBarComponent {
+
+  token:any;
+  user:any;
+  email!: string;
+  userFound: any;
+  url!: string;
+  image: any;
+  constructor(private userService:UserService,private sanitizer: DomSanitizer) {
+  }
+  ngOnInit(): void {
+    this.token = this.userService.getToken()
+    this.user = this.userService.getUser(this.token);
+    this.email = this.user.sub;
+    this.userService.findUser(this.email).subscribe((res)=>{
+      this.userFound = res.data
+      this.url ='data:image/png;base64,' + this.userFound.image.picByte;
+      this.image = this.sanitizer.bypassSecurityTrustUrl(this.url)
+    })
+  }
+  collapseShow = "hidden";
+  toggleCollapseShow(classes: string) {
+    this.collapseShow = classes;
+  }
+}
