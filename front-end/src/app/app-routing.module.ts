3cee5ed9acf3553185119998f2393342e1771685
afraklaabdelaziz <afraklaabdelaziz@gmail.com>
2023-04-05 12:06:30 +0000

    modifier liste des roles pour accedee au dashboard

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth-guard.guard';
+import { RoleGuard } from './guards/role-guard.guard';
+import { AdminComponent } from './layout/admin/admin.component';
 import { AuthComponent } from './layout/auth/auth.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import * as path from "path";
+import { LogoutComponent } from './components/logout/logout.component';
 
 const routes: Routes = [
 
@@ -14,6 +19,14 @@ const routes: Routes = [
       { path: "register", component: RegisterComponent },
       { path: "", redirectTo: "login", pathMatch: "full" },
     ],
+  },
+
+  {
+    path: "admin",
+    component: AdminComponent ,canActivate: [AuthGuard,RoleGuard],data:{role:'admin'},
+    children: [
+      {path: "logout",component: LogoutComponent}
+    ]
   },
 ];
 
